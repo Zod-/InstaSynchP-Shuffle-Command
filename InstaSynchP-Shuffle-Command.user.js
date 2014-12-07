29ab4b690e0ca5ba165630b2f7d50090fcb8962a
Julian Hangstörfer <jhangstoerfer@gmail.com>
2014-12-07 15:08:25 +0100

    Less whitespace

--- InstaSynchP-Shuffle-Command.user.js
+++ InstaSynchP-Shuffle-Command.user.js
@@ -19,51 +19,54 @@
 // ==/UserScript==
 
 function Shuffle(version) {
-    "use strict";
-    this.version = version;
-    this.name = "InstaSynchP Shuffle Command";
-    this.commands = {
-        "'shuffle": {
-            'hasArguments': true,
-            'type': 'mod',
-            'reference': this,
-            'description': 'Shuffles the playlist or a wall of a user. Parameters: the user to shuffle, "all" so that all the videos get shuffled instead of only the ones below the active one',
-            'callback': this.execute
-        }
-    };
+  "use strict";
+  this.version = version;
+  this.name = "InstaSynchP Shuffle Command";
+  this.commands = {
+    "'shuffle": {
+      'hasArguments': true,
+      'type': 'mod',
+      'reference': this,
+      'description': 'Shuffles the playlist or a wall of a user. Parameters: the user to shuffle, "all" so that all the videos get shuffled instead of only the ones below the active one',
+      'callback': this.execute
+    }
+  };
 }
 
 Shuffle.prototype.execute = function (opts) {
-    "use strict";
-    var userList = opts.usernames,
-        i,
-        tempList = [],
-        all = false;
+  "use strict";
+  var userList = opts.usernames,
+    i, video,
+    tempList = [],
+    all = false;
 
-    for (i = 1; i < arguments.length; i += 1) {
-        switch(arguments[i]){
-            case 'all': all = true; break;
-        }
+  for (i = 1; i < arguments.length; i += 1) {
+    switch (arguments[i]) {
+    case 'all':
+      all = true;
+      break;
     }
-    i = all ? 0 : activeVideoIndex() + 1;
+  }
+  i = all ? 0 : activeVideoIndex() + 1;
 
-    //save all the videos with their index
-    for (; i < window.playlist.length; i += 1) {
-        if (userList.length === 0 || userList.indexOf(window.playlist[i].addedby.toLowerCase()) !== -1) {
-            tempList.push({
-                i: i,
-                info: window.playlist[i].info
-            });
-        }
+  //save all the videos with their index
+  for (; i < window.playlist.length; i += 1) {
+    video = window.playlist[i];
+    if (userList.length === 0 || userList.indexOf(video.addedby.toLowerCase()) !== -1) {
+      tempList.push({
+        i: i,
+        info: video.info
+      });
     }
+  }
 
-    //move the videos to the position of another video in the list
-    for (i = 0; i < tempList.length; i += 1) {
-        window.global.sendcmd('move', {
-            info: tempList[i].info,
-            position: tempList[Math.floor(Math.random() * tempList.length)].i
-        });
-    }
+  //move the videos to the position of another video in the list
+  for (i = 0; i < tempList.length; i += 1) {
+    window.global.sendcmd('move', {
+      info: tempList[i].info,
+      position: tempList[Math.floor(Math.random() * tempList.length)].i
+    });
+  }
 };
 
 window.plugins = window.plugins || {};
